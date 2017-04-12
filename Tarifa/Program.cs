5fab6a412978e77942694398906e401c3cd488cd
Erik <erik.ra@gmail.com>
2017-04-12 11:32:24 +0200

    Add project files.

---
+++ Tarifa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint x = uint.Parse(Console.ReadLine());
+            uint n = uint.Parse(Console.ReadLine());
+            uint buffer = 0;
+            uint p = 0;
+            for (int i = 0; i < n; i++)
+            {
+                p = uint.Parse(Console.ReadLine());
+                buffer += x - p;
+            }
+            Console.WriteLine(buffer + x);
+        }
+    }
+}
